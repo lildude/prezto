11f705069954fe879f909513eaae12a9ccf2d8fe
Colin Seymour <lildood@gmail.com>
2014-08-16 11:10:49 +0100

    Testing using append history instead of inc append history

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -20,7 +20,8 @@ SAVEHIST=10000                   # The maximum number of events to save in the h
 
 setopt BANG_HIST                 # Treat the '!' character specially during expansion.
 setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
-setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+#setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
+setopt APPEND_HISTORY            # Only write to history file when shell exits.
 setopt SHARE_HISTORY             # Share history between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
 setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
