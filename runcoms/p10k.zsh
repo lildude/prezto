26509be001d67c17b97a0867a6b72dc2aa260e8e
Colin Seymour <colin@symr.io>
2021-03-15 12:12:39 +0000

    Truncate branch from start

--- runcoms/p10k.zsh
+++ runcoms/p10k.zsh
@@ -391,7 +391,8 @@
       # If local branch name is at most 32 characters long, show it in full.
       # Otherwise show the first 12 … the last 12.
       # Tip: To always show local branch name in full without truncation, delete the next line.
-      (( $#branch > 32 )) && branch[13,-13]="…"  # <-- this line
+      # CNS: I like 25 chars and truncation at the beginning as I prefix my branches with my handle
+      (( $#branch > 25 )) && branch[0,-26]="…"  # <-- this line
       res+="${clean}${(g::)POWERLEVEL9K_VCS_BRANCH_ICON}${branch//\%/%%}"
     fi
 
